87debc2b08a09e4c5b876405351bc8df30b4c6e0
puzanov <puzanov@gmail.com>
2017-02-15 14:55:24 +0600

    shit code on order flow but working )

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"log"
 	"gopkg.in/telegram-bot-api.v4"
 	"github.com/maddevsio/simple-config"
+	"strings"
 )
+
+type Session struct {
+	Phone string
+	Address string
+	FareId int
+	OrderStarted bool
+	OrderCreated bool
+}
 
 var (
 	config = simple_config.NewSimpleConfig("config", "yml")
+	sessions = make(map[int64]*Session)
 )
-
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(config.GetString("bot_token"))
@@ -26,6 +35,17 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		chatStateMachine(update, bot)
+	}
+}
+
+func chatStateMachine (update tgbotapi.Update, bot *tgbotapi.BotAPI) {
 	keyboard := tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton("Быстрый заказ такси"),
@@ -42,22 +62,44 @@ func main() {
 
 	keyboard.OneTimeKeyboard = true
 
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		if update.Message.Text == "Быстрый заказ такси" {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Укажите ваш телефон")
-			msg.ReplyMarkup = keyboard
-			bot.Send(msg)
+	if session := sessions[update.Message.Chat.ID]; session != nil {
+		if session.OrderStarted {
+			if strings.HasPrefix(update.Message.Text, "+996") {
+				session.Phone = update.Message.Text
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Телефон сохранен. Теперь укажите адрес")
+				bot.Send(msg)
+				return
+			} else if session.Phone != "" && session.Address == "" {
+				session.Address = update.Message.Text
+				session.OrderCreated = true
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Заказ создан!")
+				bot.Send(msg)
+				return
+			} else if session.OrderCreated {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Машина скоро будет")
+				bot.Send(msg)
+				return
+			}
 		} else {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Что-что?")
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Заказ не открыт. Откройте заново")
 			msg.ReplyToMessageID = update.Message.MessageID
 			msg.ReplyMarkup = keyboard
 			bot.Send(msg)
+			return
 		}
 	}
+
+	if update.Message.Text == "Быстрый заказ такси" {
+		sessions[update.Message.Chat.ID] = &Session{}
+		sessions[update.Message.Chat.ID].OrderStarted = true
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Укажите ваш телефон. Например: +996555112233")
+		bot.Send(msg)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Что-что?")
+	msg.ReplyToMessageID = update.Message.MessageID
+	msg.ReplyMarkup = keyboard
+	bot.Send(msg)
+	return
 }
