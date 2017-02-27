5897132a69604b3dbfdb40f5d635e9bc3f8b9642
puzanov <puzanov@gmail.com>
2017-02-27 11:59:23 +0600

    first test on gorm

--- chat/session_test.go
+++ chat/session_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestStoreSession(t *testing.T) {
@@ -16,13 +17,18 @@ func TestStoreSession(t *testing.T) {
 	// Migrate the schema
 	db.AutoMigrate(&Session{})
 
-	session := Session{}
-	session.OrderId = 1
-	session.FareId = 1
-	session.Address = "address"
-	session.Phone = "123"
-	session.State = "state"
+	session1 := Session{}
+	session1.OrderId = 1
+	session1.FareId = 1
+	session1.Address = "address"
+	session1.Phone = "123"
+	session1.State = "state"
 
 	// Create
-	db.Create(&session)
+	db.Create(&session1)
+
+	session2 := Session{}
+	db.First(&session2, "phone = ?", "123") // find product with id 1
+
+	assert.Equal(t, "123", session2.Phone)
 }
