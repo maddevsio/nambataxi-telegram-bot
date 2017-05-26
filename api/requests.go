12affa6c9170eeb9122b9d8327a08cb973cc8754
puzanov <puzanov@gmail.com>
2017-05-26 13:55:22 +0600

    consts names refactored

--- api/requests.go
+++ api/requests.go
@@ -1,80 +1,79 @@
 package api
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
-	"fmt"
-	"errors"
-	"log"
-	"encoding/json"
 	"strconv"
 )
 
 const (
-	PARTNER_ID   = "partner_id"
-	SERVER_TOKEN = "server_token"
+	PartnerID   = "partner_id"
+	ServerToken = "server_token"
 )
 
 type NambaTaxiAPI struct {
-	partnerID string
+	partnerID   string
 	serverToken string
-	url string
-	version string
+	url         string
+	version     string
 }
 
 type Fares struct {
 	Fare []struct {
-		Flagfall float64 `json:"flagfall"`
-		Free_waiting float64 `json:"free_waiting"`
-		Full_description string `json:"full_description"`
-		Include_kilometers int `json:"include_kilometers"`
-		Id int `json:"id"`
+		Flagfall           float64 `json:"flagfall"`
+		Free_waiting       float64 `json:"free_waiting"`
+		Full_description   string  `json:"full_description"`
+		Include_kilometers int     `json:"include_kilometers"`
+		Id                 int     `json:"id"`
 		Cost_per_kilometer float64 `json:"cost_per_kilometer"`
-		Name string `json:"name"`
+		Name               string  `json:"name"`
 	} `json:"fares"`
 }
 
 type PaymentMethods struct {
 	PaymentMethod []struct {
-		PaymentMethodId int `json:"payment_method_id"`
-		Description string `json:"description"`
+		PaymentMethodId int    `json:"payment_method_id"`
+		Description     string `json:"description"`
 	} `json:"payment_methods"`
 }
 
 type RequestOptions struct {
 	RequestOption []struct {
-		Id int `json:"id"`
+		Id    int    `json:"id"`
 		Title string `json:"title"`
 	} `json:"request_options"`
 }
 
 type Order struct {
-	OrderId int `json:"order_id"`
-	Message string `json:"message"`
-	Status string `json:"status"`
+	OrderId  int    `json:"order_id"`
+	Message  string `json:"message"`
+	Status   string `json:"status"`
 	TripCost string `json:"trip_cost"`
-	Driver struct {
-		Name string `json:"name"`
-		PhoneNumber string `json:"phone_number"`
-		CabNumber string `json:"cab_number"`
-		LicensePlate string `json:"license_plate"`
-		Make string `json:"make"`
-		Lat float64 `json:"lat"`
-		Lon float64 `json:"lon"`
-
+	Driver   struct {
+		Name         string  `json:"name"`
+		PhoneNumber  string  `json:"phone_number"`
+		CabNumber    string  `json:"cab_number"`
+		LicensePlate string  `json:"license_plate"`
+		Make         string  `json:"make"`
+		Lat          float64 `json:"lat"`
+		Lon          float64 `json:"lon"`
 	} `json:"driver"`
 }
 
 type Cancel struct {
-	Status string `json:"status"`
+	Status  string `json:"status"`
 	Message string `json:"message"`
 }
 
 type NearestDrivers struct {
-	Status string `json:"status"`
+	Status  string `json:"status"`
 	Message string `json:"message"`
-	Drivers int `json:"drivers"`
+	Drivers int    `json:"drivers"`
 }
 
 func NewNambaTaxiApi(partnerID string, serverToken string, url string, version string) NambaTaxiAPI {
@@ -147,7 +146,7 @@ func (api *NambaTaxiAPI) CancelOrder(id int) (Cancel, error) {
 	return structure, nil
 }
 
-func (api *NambaTaxiAPI) makePostRequestAndMapStructure(structure interface{}, uri string, postParams map[string][]string) (error) {
+func (api *NambaTaxiAPI) makePostRequestAndMapStructure(structure interface{}, uri string, postParams map[string][]string) error {
 	jsonData, err := api.makePostRequest(uri, postParams)
 	if err != nil {
 		return err
@@ -161,8 +160,8 @@ func (api *NambaTaxiAPI) makePostRequestAndMapStructure(structure interface{}, u
 
 func (api *NambaTaxiAPI) makePostRequest(uri string, postParams map[string][]string) ([]byte, error) {
 	var values url.Values = map[string][]string{
-		PARTNER_ID:   {api.partnerID},
-		SERVER_TOKEN: {api.serverToken},
+		PartnerID:   {api.partnerID},
+		ServerToken: {api.serverToken},
 	}
 
 	for key, value := range postParams {
